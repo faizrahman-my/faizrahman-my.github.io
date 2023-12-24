7c8cbbf2d75e8ac0ef0f62f39671612695238335
FaizRahman01 <mdfaiz.rahman@gmail.com>
2023-12-24 17:13:20 +0800

    apply style on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Header } from '../components/Header'
 import { Link } from 'react-router-dom'
+import arrow_right from "../assets/icons/arrow-right.svg";
 
 export const Home = () => {
   return (
@@ -11,11 +12,14 @@ export const Home = () => {
           <div className='min-md:text-5xl max-md:text-3xl'>
             <h1>Hola! I’m a recent IT graduate. </h1>
             <h1>I love stuff related to programming. </h1>
-            <h1>Feel free to explore more about <Link to={'/about'}>Faiz</Link> here.</h1>
+            <h1>Feel free to explore more about <Link to={'/about'} className='text-web-text1'>Faiz</Link> here.</h1>
           </div>
-          <div className='pt-5'>
+          <div className='mt-5 w-fit'>
             <Link to={'/project'}>
-              <p>Things I did</p>
+              <div className='flex items-center rounded-xl bg-web-both1 text-web-both3 p-3 min-md:text-xl max-md:text-lg border-2 border-web-back1'>
+                Things I did
+                <img src={arrow_right} className='ml-12' />
+              </div>
             </Link>
           </div>
         </div>
