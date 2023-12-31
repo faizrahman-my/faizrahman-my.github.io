66b5eac8315d6df8ed14cf0e410688a92a60a86b
FaizRahman01 <mdfaiz.rahman@gmail.com>
2023-12-31 14:05:33 +0800

    minor sentence change home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ export const Home = () => {
       <div className='grid content-center h-4/5 px-10'>
         <div className='font-khand'>
           <div className='min-md:text-5xl max-md:text-3xl text-web-text2'>
-            <h1>Hola! I’m a recent IT graduate. </h1>
+            <h1>Hola! I’m an IT fresh graduate. </h1>
             <h1>I love stuff related to programming. </h1>
             <h1>Feel free to explore more about <Link to={'/about'} className='text-web-text1 hover:text-web-text2'>Faiz</Link> here.</h1>
           </div>
